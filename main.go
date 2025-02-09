9e95a334d3b676fc8d20472cb68e99ef74e05b76
Risfat97 <tamsir97@gmail.com>
2025-02-09 17:37:18 +0100

    The standard library

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
-	// Arrays
-	students := [5]string{"John", "Jane", "Eric", "Louise", "Alice"}
-	ratings := [5]float32{7.25, 9, 7, 8.5, 9.75}
-	students[2] = "Bob"
-	ratings[len(ratings)-1] = 8.75
+	// strings
+	quote := "Socrate: All I know is that I don't know anything."
+	fmt.Println(strings.Contains(quote, "Socrate")) // true
+	fmt.Println(strings.Replace(quote, "Socrate", "Sirtaf", 1))
+	fmt.Println(strings.Split(quote, " "))
 
-	// You can't do this
-	// students[6] = "Milo"   // index 6 out of bounds [0:5]
-	// students[0] = 123   // 123 is not a string
-
-	fmt.Println(students, ratings)
-
-	// Slices
-	employees := []string{"Bob", "Alice", "John", "Louise"}
-	employees = append(employees, "Milo")
-	fmt.Println(employees)
-	fmt.Println("Employees for the morning are:", employees[:2])
-	fmt.Println("Employees for the evening are:", employees[2:])
-
-	// Arrays and slices
-	students1 := students[:2]
-	fmt.Println(students, students1)
-	students1 = append(students1, "Marc", "Toby")
-	fmt.Println(students, students1) // Output: [John Jane Marc Toby Alice] [John Jane Marc Toby]
-	fmt.Println("cap(students1) =", cap(students1))
-
-	// Creating slice with make
-	score := make([]int, 3, 10) // score = [0 0 0]
-	score = append(score, 25)   // score [0 0 0 25]
-	fmt.Println(score, len(score), cap(score))
+	// sort
+	ratings := []int{12, 17, 9, 15, 11, 8, 7, 19}
+	names := []string{"john", "mike", "alice", "bob"}
+	sort.Ints(ratings)
+	sort.Strings(names)
+	index1 := sort.SearchInts(ratings, 11)      // Reasearch the value in the sorted slice
+	index2 := sort.SearchStrings(names, "jane") // Returns 2 because it's the position to insert 'jane' to keep the slice sorted
+	fmt.Println(index1, index2)
 }
